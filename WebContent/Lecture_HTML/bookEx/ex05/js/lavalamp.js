1d985fde43fc76ac15ae5b6e3e15e91ccea5781f
Mirmilcass <jaweoan9@gmail.com>
2015-10-13 17:21:39 +0900

    15.10.13 ex5 ex8 제대로 되지 않음. 찾아야함.

--- WebContent/Lecture_HTML/bookEx/ex05/js/lavalamp.js
+++ WebContent/Lecture_HTML/bookEx/ex05/js/lavalamp.js
@@ -2,8 +2,52 @@
 	$(function() {
 
 		var $nav = $('#navigation'), 
-			$current_item = $nav.find('.focus'), 
-			$lava = $('<li class="lava"/>');
+		$current_item = $nav.find('.focus'), 
+		$lava = $('<li class="lava"/>');
+		$lava.appendTo($nav.find('ul'));
+
+		options = {
+			gap : 20,
+			speed : 400,
+			easing : 'easeInOutElastion',
+			reset : 2000
+		};
+
+		$nav.css('position', 'relative').find('a').css({
+			position : 'relative',
+			zIndex : 1
+		});
+//		$nav.find('li').bind('mouseover focusin', function() {
+//			clearTimeout(reset);
+//			$lava.aniamate({
+//				left : $(this).position().left,
+//				width : $(this).outerWidth()
+//			}, {
+//				duration : options.speed,
+//				eastion : options.easing,
+//				queue : false
+//			});
+//		}).bind('mouseout focustout', function() {
+//			reset = setTimeout(function() {
+//				$lava.animate({
+//
+//					left : $current_item.position().left,
+//					width : $current_item.outerWidth()
+//				}, options.speed);
+//			}, options.reset);
+//		});
+
+		$lava.css({
+			Position : 'absolute',
+			top : $current_item.outerWidth().top - options.gap / 2,
+			left : $current_item.outerHeight().left,
+
+			width : $current_item.outerWidth(),
+
+			height : $current_item.outerHeight() + options.gap,
+
+			backgroundColor : '#eeeeee'
+		}).appendTo($nav.find('ul'));
 
 	});
 
