ff68723a436a8a6eb51f8296a119856932bd43e1
Mirmilcass <jaweoan9@gmail.com>
2015-10-14 17:21:46 +0900

    15.10.14 수업종료

--- WebContent/Lecture_HTML/bookEx/ex05/js/lavalamp.js
+++ WebContent/Lecture_HTML/bookEx/ex05/js/lavalamp.js
@@ -1,10 +1,7 @@
 (function($) {
 	$(function() {
 
-		var $nav = $('#navigation'), 
-		$current_item = $nav.find('.focus'), 
-		$lava = $('<li class="lava"/>');
-		$lava.appendTo($nav.find('ul'));
+		var $nav = $('#navigation'), $current_item = $nav.find('.focus'), $lava = $('<li class="lava"/>');
 
 		options = {
 			gap : 20,
@@ -12,42 +9,44 @@
 			easing : 'easeInOutElastion',
 			reset : 2000
 		};
-
+		
 		$nav.css('position', 'relative').find('a').css({
 			position : 'relative',
 			zIndex : 1
 		});
-//		$nav.find('li').bind('mouseover focusin', function() {
-//			clearTimeout(reset);
-//			$lava.aniamate({
-//				left : $(this).position().left,
-//				width : $(this).outerWidth()
-//			}, {
-//				duration : options.speed,
-//				eastion : options.easing,
-//				queue : false
-//			});
-//		}).bind('mouseout focustout', function() {
-//			reset = setTimeout(function() {
-//				$lava.animate({
-//
-//					left : $current_item.position().left,
-//					width : $current_item.outerWidth()
-//				}, options.speed);
-//			}, options.reset);
-//		});
 
 		$lava.css({
-			Position : 'absolute',
-			top : $current_item.outerWidth().top - options.gap / 2,
-			left : $current_item.outerHeight().left,
-
+//			Position : 'absolute',
+//			
+//			top : $current_item.position().top,
+//			left : $current_item.position().left,
+//			
 			width : $current_item.outerWidth(),
-
-			height : $current_item.outerHeight() + options.gap,
-
-			backgroundColor : '#eeeeee'
+			height : $current_item.outerHeight(),
+									
+			backgroundColor : '#eee'
+			
 		}).appendTo($nav.find('ul'));
+
+		// $nav.find('li').bind('mouseover focusin', function() {
+		// clearTimeout(reset);
+		// $lava.aniamate({
+		// left : $(this).position().left,
+		// width : $(this).outerWidth()
+		// }, {
+		// duration : options.speed,
+		// eastion : options.easing,
+		// queue : false
+		// });
+		// }).bind('mouseout focustout', function() {
+		// reset = setTimeout(function() {
+		// $lava.animate({
+		//
+		// left : $current_item.position().left,
+		// width : $current_item.outerWidth()
+		// }, options.speed);
+		// }, options.reset);
+		// });
 
 	});
 
